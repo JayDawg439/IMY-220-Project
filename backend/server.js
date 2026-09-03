2fbdc67b4eb57070d7294d4ed5b5c2275295faf8
JayDawg439 <jaydenwaltersmith@gmail.com>
2026-09-03 12:42:06 +0200

    Part 1

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,42 @@
-const express = require;
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
-app.use(express.static("public"));
-app.listen(1337, () => {
-    console.log("Listening on localhost:1337");
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('Running');
+});
+
+app.post('/api/signup', (req, res) => {
+    const { username, email, password } = req.body;
+
+    res.status(201).json({
+        message: 'User created',
+        user: {
+            id: 'id',
+            username: username || 'user',
+            email: email || 'user@email.com'
+        }
+    })
+});
+
+app.post('/api/signin', (req, res) => {
+    const { email, password } = req.body;
+
+    res.status(200).json({
+        message: 'Login successful',
+        user: {
+            id: 'id',
+            username: 'user',
+            email: email || 'user@email.com'
+        }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
